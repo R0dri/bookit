665a891a21203d0ed6dbdb2a91dc8615a1aa40fd
rodri <rodri.mendoza.t@gmail.com>
2025-07-13 13:32:10 -0400

    ionic serve on github add folder

---
+++ book-it/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Book It!',
+  webDir: 'dist'
+};
+
+export default config;
